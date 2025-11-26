cc1dd2a480ee1684afea4ef2b1f6bb75bf011ddb
Rohan Solse <rohan.solse@transsight.com>
2025-11-26 17:00:31 +0530

    updated

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -53,106 +53,106 @@ const resList = [
   },
    {
     "info": {
-      "id": "334479",
-      "name": "KFC",
-      "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
-      "avgRating": 4.1,
-      "cuisines": ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      "id": "334487",
+      "name": "Subway",
+      "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+      "avgRating": 4.2,
+      "cuisines": ["Salads", "Snacks", "Desserts", "Beverages"],
       "sla": {
-        "deliveryTime": 36
+        "deliveryTime": 30
       },
       "orderCount": "5000+ orders"
     }
   },
   {
     "info": {
-      "id": "334480",
-      "name": "McDonald's",
-      "cloudinaryImageId": "bb7ae131544c7d37e10fc5faf76f09d6",
-      "avgRating": 4.3,
+      "id": "334488",
+      "name": "Pizza Hut",
+      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      "avgRating": 3.9,
+      "cuisines": ["Pizzas"],
+      "sla": {
+        "deliveryTime": 35
+      },
+      "orderCount": "5000+ orders"
+    }
+  },
+  {
+    "info": {
+      "id": "334489",
+      "name": "Burger King",
+      "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+      "avgRating": 4.2,
       "cuisines": ["Burgers", "American"],
       "sla": {
-        "deliveryTime": 25
+        "deliveryTime": 38
       },
       "orderCount": "10k+ orders"
     }
   },
   {
     "info": {
-      "id": "334481",
-      "name": "Domino's Pizza",
-      "cloudinaryImageId": "d0450ce1a6ba19ea60cd724471ed54a8",
-      "avgRating": 4.5,
-      "cuisines": ["Pizza", "Italian", "Pastas", "Desserts"],
+      "id": "334490",
+      "name": "Wow! Momo",
+      "cloudinaryImageId": "0984acc0ed7b914206dbbcb90297becc",
+      "avgRating": 4.3,
+      "cuisines": ["Tibetan", "Chinese", "Snacks"],
       "sla": {
-        "deliveryTime": 20
+        "deliveryTime": 28
       },
-      "orderCount": "10k+ orders"
-    }
-  },
-  {
-    "info": {
-      "id": "334482",
-      "name": "Street Food Corner",
-      "cloudinaryImageId": "c9elzh8awx7jcx70cood",
-      "avgRating": 3.8,
-      "cuisines": ["Snacks", "Chaat"],
-      "sla": {
-        "deliveryTime": 45
-      },
-      "orderCount": "1000+ orders"
+      "orderCount": "5000+ orders"
     }
   },
    {
     "info": {
-      "id": "334483",
-      "name": "KFC",
-      "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
-      "avgRating": 4.1,
-      "cuisines": ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      "id": "334491",
+      "name": "Baskin Robbins",
+      "cloudinaryImageId": "85ccae4e3576f9330af102c46ca85395",
+      "avgRating": 4.6,
+      "cuisines": ["Desserts", "Ice Cream"],
       "sla": {
-        "deliveryTime": 36
+        "deliveryTime": 15
+      },
+      "orderCount": "1000+ orders"
+    }
+  },
+  {
+    "info": {
+      "id": "334492",
+      "name": "Taco Bell",
+      "cloudinaryImageId": "a902ca3541554c25785039a824471e52",
+      "avgRating": 4.2,
+      "cuisines": ["Mexican", "Wraps", "Fast Food"],
+      "sla": {
+        "deliveryTime": 33
+      },
+      "orderCount": "10k+ orders"
+    }
+  },
+  {
+    "info": {
+      "id": "334493",
+      "name": "Faasos - Wraps & Rolls",
+      "cloudinaryImageId": "af33b81798b11deba338e94b7585d348",
+      "avgRating": 4.2,
+      "cuisines": ["Wraps", "Desserts", "Beverages"],
+      "sla": {
+        "deliveryTime": 29
       },
       "orderCount": "5000+ orders"
     }
   },
   {
     "info": {
-      "id": "334484",
-      "name": "McDonald's",
-      "cloudinaryImageId": "bb7ae131544c7d37e10fc5faf76f09d6",
-      "avgRating": 4.3,
-      "cuisines": ["Burgers", "American"],
+      "id": "334494",
+      "name": "Behrouz Biryani",
+      "cloudinaryImageId": "89fccaa76f2f760e2742b9e53d32bb69",
+      "avgRating": 4.4,
+      "cuisines": ["Biryani", "Mughlai", "North Indian"],
       "sla": {
-        "deliveryTime": 25
+        "deliveryTime": 40
       },
-      "orderCount": "10k+ orders"
-    }
-  },
-  {
-    "info": {
-      "id": "334485",
-      "name": "Domino's Pizza",
-      "cloudinaryImageId": "d0450ce1a6ba19ea60cd724471ed54a8",
-      "avgRating": 4.5,
-      "cuisines": ["Pizza", "Italian", "Pastas", "Desserts"],
-      "sla": {
-        "deliveryTime": 20
-      },
-      "orderCount": "10k+ orders"
-    }
-  },
-  {
-    "info": {
-      "id": "334486",
-      "name": "Street Food Corner",
-      "cloudinaryImageId": "c9elzh8awx7jcx70cood",
-      "avgRating": 3.8,
-      "cuisines": ["Snacks", "Chaat"],
-      "sla": {
-        "deliveryTime": 45
-      },
-      "orderCount": "1000+ orders"
+      "orderCount": "5000+ orders"
     }
   }
 ];
